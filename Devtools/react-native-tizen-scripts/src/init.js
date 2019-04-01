283bc8827de6e07caa4a88416dff1fa21ce24863
sutaking <zhaofeng.vip@gmail.com>
2019-04-01 18:57:46 +0800

    update template

--- Devtools/react-native-tizen-scripts/src/init.js
+++ Devtools/react-native-tizen-scripts/src/init.js
@@ -62,12 +62,10 @@ https://github.com/npm/npm/issues/16991
         appPackage.scripts = {
             server: 'node node_modules/react-native/local-cli/cli.js start',
             test: 'node node_modules/jest/bin/jest.js --watch',
-            package: 'react-native-tizen package $npm_package_config_mode',
+            package: 'react-native-tizen package',
             bundle: 'react-native-tizen bundle',
             //dotnet: 'react-native-tizen dotnet',
-            launch: 'react-native-tizen launch $npm_package_config_tvip $npm_package_config_mode'
-                //packageDebug:'yarn bundle -- dev; yarn dotnet -- debug; yarn package',
-                //packageRelease: 'yarn bundle; yarn dotnet -- release; yarn package',
+            launch: 'react-native-tizen launch'
         };
 
         appPackage.jest = {};
